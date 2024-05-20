2fb8f918f63286fadcaa350b3d0c22348f255546
Azreal7 <923052197@qq.com>
2024-05-20 14:29:02 +0800

    threadpool

--- src/Server.cpp
+++ src/Server.cpp
@@ -4,18 +4,29 @@
 #include "Channel.h"
 #include "Acceptor.h"
 #include "Connection.h"
+#include "EventLoop.h"
+#include "ThreadPool.h"
 #include <functional>
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <errno.h>
+#include <thread>
 
 #define READ_BUFFER 1024
 
-Server::Server(EventLoop *_loop):loop(_loop), acceptor(nullptr) {
-    acceptor = new Acceptor(loop);
+Server::Server(EventLoop *_loop):mainReactor(_loop), acceptor(nullptr) {
+    acceptor = new Acceptor(mainReactor);
     std::function<void(Socket*)> cb = std::bind(&Server::newConnection, this, std::placeholders::_1);
     acceptor->setNewConnectionCallback(cb);
+
+    int size = std::thread::hardware_concurrency();
+    thpool = new ThreadPool(size);
+    for(int i = 0; i < size; ++i) {
+        subReactors.push_back(new EventLoop());
+        std::function<void()> sub_loop = std::bind(&EventLoop::loop, subReactors[i]);
+        thpool->add(sub_loop);
+    }
 }
 
 Server::~Server() {
@@ -24,7 +35,10 @@ Server::~Server() {
 
 void Server::newConnection(Socket *sock) {
     if(sock->getFd() != -1) {
-        Connection *conn = new Connection(loop, sock);
+        // 这一部分调度算法可重新设计
+        int random = sock->getFd() % subReactors.size();
+        Connection *conn = new Connection(subReactors[random], sock);
+        
         std::function<void(Socket*)> cb = std::bind(&Server::deleteConnection, this, std::placeholders::_1);
         conn->setDeleteConnectionCallback(cb);
         connections[sock->getFd()] = conn;
